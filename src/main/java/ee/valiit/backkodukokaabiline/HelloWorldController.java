7fe106c46b145beef03328b5b7e910ac96b858c0
HelenSo <helensoodla@gmail.com>
2022-11-14 14:19:03 +0200

    develop

--- src/main/java/ee/valiit/backkodukokaabiline/HelloWorldController.java
+++ src/main/java/ee/valiit/backkodukokaabiline/HelloWorldController.java
@@ -1,2 +1,13 @@
-package ee.valiit.backkodukokaabiline;public class HelloWorldController {
+package ee.valiit.backkodukokaabiline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/world")
+    public String helloWorld() {
+        return "HELLO WOOOOORLD!!! YEE!";
+    }
 }
